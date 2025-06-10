9198178d8a4bd69d211a06589daa27383401a517
aslanismailov <i@aslanismailov.ru>
2025-06-10 18:32:21 +0300

    added navigation

---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,13 @@
+import './Navigation.css';
+
+const Navigation = () => {
+    return (
+        <div className="navigation">
+            <a href="/">Tasks</a>
+            <a href="/rewards">Rewards</a>
+            <a href="/profile">Profile</a>
+        </div>
+    );
+};
+
+export { Navigation };
