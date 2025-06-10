1c93ae7b857a9f9128c521769b3cc117bd507d35
aslanismailov <i@aslanismailov.ru>
2025-06-10 18:30:30 +0300

    added react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
-import { TasksPage } from './pages/TasksPage';
+import { TasksPage } from './pages/tasks-page/TasksPage.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Navigation } from './components/navigation/Navigation.tsx';
+
 import './App.css';
+
+const queryClient = new QueryClient();
 
 const App = () => {
     return (
-        <>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<TasksPage />} />
-                </Routes>
-            </Router>
-        </>
+        <div className="app">
+            <QueryClientProvider client={queryClient}>
+                <div className="layout">
+                    <main className="page-container">
+                        <Router>
+                            <Routes>
+                                <Route path="/" element={<TasksPage />} />
+                            </Routes>
+                        </Router>
+                    </main>
+                    <Navigation />
+                </div>
+            </QueryClientProvider>
+        </div>
     );
 };
 
